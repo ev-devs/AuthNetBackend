2f0e9e439f07daecfe7cee8f251f69b661282974
jruel006 <jruel006@ucr.edu>
2016-08-22 13:49:49 -0700

    forgot to terminate the comment

--- AuthNetTransactions/charge-credit-card.rb
+++ AuthNetTransactions/charge-credit-card.rb
@@ -16,7 +16,7 @@ request = CreateTransactionRequest.new
 request.transactionRequest = TransactionRequestType.new()
 request.transactionRequest.amount = Amount
 request.transactionRequest.payment = PaymentType.new
-request.transactionRequest.payment.creditCard = CreditCardType.new(CreditCardNumber, ExpirationDate # ,CCV)
+request.transactionRequest.payment.creditCard = CreditCardType.new(CreditCardNumber, ExpirationDate)
 request.transactionRequest.transactionType = TransactionTypeEnum::AuthCaptureTransaction
 
 response = transaction.create_transaction(request)
